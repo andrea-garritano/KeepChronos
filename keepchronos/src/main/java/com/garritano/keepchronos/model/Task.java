22816e2bb4a91ad1ff91314f4abfb2bcf889e4f3
Andrea Garritano <6050435+andryxxx@users.noreply.github.com>
2018-06-07 16:55:46 +0200

    Task entity

--- keepchronos/src/main/java/com/garritano/keepchronos/model/Task.java
+++ keepchronos/src/main/java/com/garritano/keepchronos/model/Task.java
@@ -1,8 +1,19 @@
 package com.garritano.keepchronos.model;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Task extends BasicEntity{
 	
+	private Project project;
+	
+	@ManyToOne
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
 }
