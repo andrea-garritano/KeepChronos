9a6f0b601579f104a118b38d8bb8d3b5d7149e0b
Andrea Garritano <6050435+andryxxx@users.noreply.github.com>
2018-06-14 21:44:20 +0200

    Remove inheritance of project because the presence of primary key

--- keepchronos/src/main/java/com/garritano/keepchronos/model/nosql/Project.java
+++ keepchronos/src/main/java/com/garritano/keepchronos/model/nosql/Project.java
@@ -1,21 +1,60 @@
 package com.garritano.keepchronos.model.nosql;
 
 import javax.persistence.Embeddable;
-import javax.persistence.GeneratedValue;
 
 import org.hibernate.search.annotations.Indexed;
 
-import com.garritano.keepchronos.model.BasicEntity;
-
 @Embeddable
 @Indexed
-public class Project extends BasicEntity {
+public class Project{
+	
+	private String title;
+	private String description;
 
-	private Long id;
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 	@Override
-	@GeneratedValue
-	public Long getId() {
-		return id;
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Project other = (Project) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
 	}
 }
