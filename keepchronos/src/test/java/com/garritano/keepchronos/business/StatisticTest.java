69830db12bec6cf3fd9bebc70e52598a04ea6306
Andrea Garritano <6050435+andryxxx@users.noreply.github.com>
2018-06-17 11:23:50 +0200

    Test number of Projects

--- keepchronos/src/test/java/com/garritano/keepchronos/business/StatisticTest.java
+++ keepchronos/src/test/java/com/garritano/keepchronos/business/StatisticTest.java
@@ -1,19 +1,29 @@
 package com.garritano.keepchronos.business;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.garritano.keepchronos.dao.ProjectDaoInterface;
+import com.garritano.keepchronos.model.Project;
 
 public class StatisticTest {
 	private Statistic statistic;
 	private ProjectDaoInterface projectDaoInterface;
+	private List<Project> projects;
 
 	@Before
 	public void setUp() throws Exception {
+		projects = new ArrayList<Project>();
+		projectDaoInterface = mock(ProjectDaoInterface.class);
+		when(projectDaoInterface.getAll()).thenReturn(projects);
+		
 		statistic = new Statistic(projectDaoInterface);
 	}
 
@@ -21,10 +31,23 @@ public class StatisticTest {
 		int numberOfProject = statistic.getNumberOfProject();
 		assertEquals(expected, numberOfProject);
 	}
-	
+
 	@Test
 	public void testNoProject() {
 		assertNumberOfProject(0);
+	}
+
+	@Test
+	public void testSingleProject() {
+		projects.add(new Project());
+		assertNumberOfProject(1);
+	}
+	
+	@Test
+	public void testMultipleProjects() {
+		projects.add(new Project());
+		projects.add(new Project());
+		assertNumberOfProject(2);
 	}
 
 	@After
