f04294d3af26c0e5549eb8147e00b326d48e97b9
Andrea Garritano <6050435+andryxxx@users.noreply.github.com>
2018-06-17 14:39:41 +0200

    Implement total duration of a project

--- keepchronos/src/main/java/com/garritano/keepchronos/business/ProjectStatistic.java
+++ keepchronos/src/main/java/com/garritano/keepchronos/business/ProjectStatistic.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import com.garritano.keepchronos.dao.ProjectDaoInterface;
 import com.garritano.keepchronos.model.Project;
+import com.garritano.keepchronos.model.Task;
 
 public class ProjectStatistic {
 	private ProjectDaoInterface projectDaoInterface;
-	
+
 	public ProjectStatistic(ProjectDaoInterface projectDaoInterface) {
 		super();
 		this.projectDaoInterface = projectDaoInterface;
@@ -15,7 +16,16 @@ public class ProjectStatistic {
 
 	public int getNumberOfProject() {
 		List<Project> projects = projectDaoInterface.getAll();
-		
+
 		return projects.size();
+	}
+
+	public int getTotalDuration(Project tempProject) {
+		List<Task> tasks = projectDaoInterface.getTasks(tempProject);
+		int sum = 0;
+		for (Task task : tasks) {
+			sum += task.getDuration();
+		}
+		return sum;
 	}
 }
--- keepchronos/src/test/java/com/garritano/keepchronos/business/ProjectStatisticTest.java
+++ keepchronos/src/test/java/com/garritano/keepchronos/business/ProjectStatisticTest.java
@@ -11,18 +11,24 @@ import org.junit.Test;
 
 import com.garritano.keepchronos.dao.ProjectDaoInterface;
 import com.garritano.keepchronos.model.Project;
+import com.garritano.keepchronos.model.Task;
 
 public class ProjectStatisticTest {
 	private ProjectStatistic statistic;
 	private ProjectDaoInterface projectDaoInterface;
 	private List<Project> projects;
+	private ArrayList<Task> tasks;
 
 	@Before
 	public void setUp() throws Exception {
 		projects = new ArrayList<Project>();
 		projectDaoInterface = mock(ProjectDaoInterface.class);
 		when(projectDaoInterface.getAll()).thenReturn(projects);
-		
+
+		tasks = new ArrayList<Task>();
+
+		when(projectDaoInterface.getTasks(any(Project.class))).thenReturn(tasks);
+
 		statistic = new ProjectStatistic(projectDaoInterface);
 	}
 
@@ -41,11 +47,50 @@ public class ProjectStatisticTest {
 		projects.add(new Project());
 		assertNumberOfProject(1);
 	}
-	
+
 	@Test
 	public void testMultipleProjects() {
 		projects.add(new Project());
 		projects.add(new Project());
 		assertNumberOfProject(2);
+	}
+
+	private void assertTotalDuration(Project project, int expected) {
+		int averageDuration = statistic.getTotalDuration(project);
+		assertEquals(expected, averageDuration);
+	}
+
+	@Test
+	public void testGetTotalDurationGivenProjectWithoutTask() {
+		Project tempProject = new Project();
+
+		assertTotalDuration(tempProject, 0);
+	}
+
+	@Test
+	public void testGetTotalDurationGivenProjectWithOneTask() {
+		Project tempProject = new Project();
+
+		Task tempTask1 = new Task();
+		tempTask1.setDuration(30);
+
+		tasks.add(tempTask1);
+
+		assertTotalDuration(tempProject, 30);
+	}
+
+	@Test
+	public void testGetTotalDurationGivenProject() {
+		Project tempProject = new Project();
+		Task tempTask1 = new Task();
+		tempTask1.setDuration(30);
+
+		Task tempTask2 = new Task();
+		tempTask2.setDuration(20);
+
+		tasks.add(tempTask1);
+		tasks.add(tempTask2);
+
+		assertTotalDuration(tempProject, 50);
 	}
 }
